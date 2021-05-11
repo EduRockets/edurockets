75f97588241b87b88b76eb9538786ae4108b4c4b
carlosvhz <carlosvhz@outlook.com>
2021-05-11 16:48:03 -0600

    cositas

--- edurockets-server/.eslintrc.js
+++ edurockets-server/.eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-  extends: 'standard',
   env: {
     node: true,
     commonjs: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'prettier', 'airbnb-base'],
-  plugins: ['prettier'],
+  extends: ["eslint:recommended", "prettier", "airbnb-base"],
+  plugins: ["prettier"],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    "prettier/prettier": "error",
     "no-underscore-dangle": ["error", { allow: ["_id"] }],
   },
 };
