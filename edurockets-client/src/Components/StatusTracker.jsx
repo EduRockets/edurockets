a9800777732e94d98f5e4861f85795af1d00cf47
carlosvhz <carlosvhz@outlook.com>
2021-05-31 17:23:07 -0600

    estructura del tracker

--- edurockets-client/src/Components/StatusTracker.jsx
+++ edurockets-client/src/Components/StatusTracker.jsx
@@ -2,15 +2,66 @@ import React from 'react';
 
 import { Col, Row } from 'reactstrap';
 
+import DivButton from '../Components/DivButton';
+
 import './Styles/StatusTracker.css';
 
 const StatusTracker = ({ requirements }) => {
   return (
     <div className="StatusTracker">
       <Row>
-        <Col>Requisitos</Col>
+        <Col>
+          <div className="StatusTrackerTitle">Requisitos</div>
+        </Col>
       </Row>
-      <div></div>
+      <Row>
+        <Col lg="3">
+          <div className="StatusTrackerDivider" />
+        </Col>
+        <Col />
+      </Row>
+      <Row>
+        <Col lg="3">
+          <div className="StatusTrackerIndicator StatusTrackerIndicatorCheked" />
+        </Col>
+        <Col className="StatusTrackerText">Pasaporte | ID</Col>
+      </Row>
+      <Row>
+        <Col lg="3">
+          <div className="StatusTrackerDivider" />
+        </Col>
+        <Col />
+      </Row>
+      <Row>
+        <Col lg="3">
+          <div className="StatusTrackerIndicator" />
+        </Col>
+        <Col className="StatusTrackerText">Carta de solicitud</Col>
+      </Row>
+      <Row>
+        <Col lg="3">
+          <div className="StatusTrackerDivider" />
+        </Col>
+        <Col />
+      </Row>
+      <Row>
+        <Col lg="3">
+          <div className="StatusTrackerIndicator" />
+        </Col>
+        <Col className="StatusTrackerText">Ensayo</Col>
+      </Row>
+      <Row>
+        <Col lg="3">
+          <div className="StatusTrackerDivider" />
+        </Col>
+        <Col />
+      </Row>
+      <Row>
+        <Col lg="3">
+          <div className="StatusTrackerIndicator" />
+        </Col>
+        <Col className="StatusTrackerText">Curriculum</Col>
+      </Row>
     </div>
   );
 };
